ddb24d2ecfa12815f12d1089122082856e741141
FabiolaCop33 <fabiola.padilla@sacrecoeur.group>
2024-10-17 20:03:30 -0600

    Initial commit for weather-report-api
    :wq#
    ~

---
+++ src/main/java/com/dealengine/weather/weather_report_api/WeatherReportApiApplication.java
@@ -0,0 +1,13 @@
+package com.dealengine.weather.weather_report_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherReportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherReportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dealengine/weather/weather_report_api/WeatherReportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealengine.weather.weather_report_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherReportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
